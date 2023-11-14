c880149f8414d7de56178cc9d3c56a01bed6a46f
Spydirwebb <webb.jeffery.89@gmail.com>
2023-11-14 00:47:48 +0000

    most of the ui is rendered

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
-        <div className="flex max-xl flex-col gap-16">
+      <section className="px-6 md:px-20 py-24 ">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
@@ -21,9 +23,9 @@ const Home = () => {
             <p className="mt-6">
               Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
-            SearchBar
+            <Searchbar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
       <section className="trending-section">
