90b4026b0a1594352544fe3c0cadfb6064eb4b12
Spydirwebb <webb.jeffery.89@gmail.com>
2023-11-16 00:59:06 +0000

    server functions working

--- PROGRESS.md
+++ PROGRESS.md
@@ -1,2 +1,3 @@
 09nov 20:09 - initialized app.
 13nov 56:26 - most ui is rendered
+15nov 1:18:36 - scraped successfully
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,9 +1,49 @@
 'use client'
-import React from 'react'
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import React, { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url)
+    const hostName = parsedURL.hostname;
+
+    if (
+      hostName.includes('amazon.com') || 
+      hostName.includes('amazon.') ||
+      hostName.includes('amazon')
+    )
+    {
+      return true
+    }
+  } catch (error) {
+    return false
+  }
+
+  return false
+}
 
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
   
-  const handleSubmit = () => {}
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+    if(!isValidLink) return alert('Please enter a valid Amazon Product URL')
+
+    try{
+      setIsLoading(true)
+
+      //scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt)
+    } catch (error){
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <form 
@@ -11,13 +51,16 @@ const Searchbar = () => {
         onSubmit={handleSubmit}
     >
         <input type='text'
+            value={searchPrompt}
+            onChange={(e) => setSearchPrompt(e.target.value)}
             placeholder='Enter product link'
             className='searchbar-input'
         />
         <button 
             type='submit'
-            className='searchbar-btn'>
-            Search
+            className='searchbar-btn'
+            disabled={searchPrompt === ''}>
+            {isLoading ? "Searching..." : "Search"}
         </button>
     </form>
 
